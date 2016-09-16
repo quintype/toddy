e092437989b32cc5c38aa6767b010bf609c3e581
Tejas Dinkar <tejas@gja.in>
2016-09-16 10:40:01 +0530

    Renaming toddy to progressive

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var util = require('gulp-quintype')
 var del = require('del');
 
-var destination = "public/toddy/assets";
+var destination = "public/progressive/assets";
 
 function compileAll() {
   return util.manifestAndWrite(destination, util.merge(
